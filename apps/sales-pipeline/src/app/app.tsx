ef65f9387f60d357ea45f67983cca74c73335085
Andres Valencia <andresvalencia.diaz@davita.com>
2022-09-23 10:01:34 -0500

    feat(app-sales-pipeline): setting up router

--- apps/sales-pipeline/src/app/app.tsx
+++ apps/sales-pipeline/src/app/app.tsx
@@ -1,6 +1,18 @@
+import './app.scss';
+import { SalesPipelineFeatureConvert } from '@sales-pipeline/sales-pipeline/feature-convert';
+import { SalesPipelineFeatureHome } from '@sales-pipeline/sales-pipeline/feature-home';
+import { Route, Routes } from 'react-router-dom';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<SalesPipelineFeatureHome />} />
+        <Route path="convert" element={<SalesPipelineFeatureConvert />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
