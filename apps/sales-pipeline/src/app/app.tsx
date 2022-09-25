9cf2b2fac91c1057bf6130a21722a90c6ecef25a
Andres Valencia <andresvalencia.diaz@davita.com>
2022-09-25 02:18:49 -0500

    fix(app): setting css module for app component

--- apps/sales-pipeline/src/app/app.tsx
+++ apps/sales-pipeline/src/app/app.tsx
@@ -1,4 +1,4 @@
-import './app.scss';
+import styles from './app.module.scss';
 import { SalesPipelineFeatureConvert } from '@sales-pipeline/sales-pipeline/feature-convert';
 import { SalesPipelineFeatureHome } from '@sales-pipeline/sales-pipeline/feature-home';
 import { Route, Routes } from 'react-router-dom';
@@ -6,7 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function App() {
   return (
-    <div className="app-container">
+    <div className={styles['app-container']}>
       <Routes>
         <Route path="/" element={<SalesPipelineFeatureHome />} />
         <Route path="convert" element={<SalesPipelineFeatureConvert />} />
