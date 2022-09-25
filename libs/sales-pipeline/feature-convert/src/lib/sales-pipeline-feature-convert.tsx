f55de97d953bb62ee5cfdf67e12b70cc59180383
Andres Valencia <andresvalencia.diaz@davita.com>
2022-09-24 23:44:04 -0500

    feat(lib-f-convert): handling lead selection + setting convert process component

--- libs/sales-pipeline/feature-convert/src/lib/sales-pipeline-feature-convert.tsx
+++ libs/sales-pipeline/feature-convert/src/lib/sales-pipeline-feature-convert.tsx
@@ -1,10 +1,18 @@
-import { Lead } from '@sales-pipeline/data';
+import { CombinedScoreResponse, Lead } from '@sales-pipeline/data';
 import { getLead } from '@sales-pipeline/data-access';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './sales-pipeline-feature-convert.scss';
 import { Subject, takeUntil } from 'rxjs';
 import Prospect from './components/prospects/prospects';
+import { processLeadConvert } from './services/convert.service';
+import ConvertProcess from './components/convert-process/convert-process';
+
+const scoreInitialState = {
+  score: 0,
+  requestErrors: [],
+  systemsErrors: [],
+};
 
 /* eslint-disable-next-line */
 export interface SalesPipelineFeatureConvertProps {}
@@ -13,32 +21,64 @@ export const SalesPipelineFeatureConvert: React.FC<
   SalesPipelineFeatureConvertProps
 > = () => {
   const $destroy = new Subject<boolean>();
-  const [lead, setLead] = useState<Lead>();
+  const [lead, setLead] = useState<Lead | null>(null);
   const [searchId, setSearchId] = useState('');
+  const [scoreInProcess, setScoreInProcess] = useState(false);
+  const [score, setScore] = useState<CombinedScoreResponse>(scoreInitialState);
 
-  // useEffect(() => {
-  //   return () => {
-  //     $destroy.next(true);
-  //     $destroy.unsubscribe();
-  //   };
-  // }, []);
+  useEffect(() => {
+    return () => {
+      $destroy.next(true);
+      $destroy.unsubscribe();
+    };
+  }, []);
   const navigate = useNavigate();
 
   const goToHome = () => {
     navigate('/');
   };
 
+  const hasLeadSelected = () => {
+    return !!lead;
+  };
+
+  const hasScore = () => {
+    return lead ? lead.hasScore : false;
+  };
+
   const getLeadById = (id: string) => {
+    setScore(scoreInitialState);
+    setScoreInProcess(false);
     getLead(id)
       .pipe(takeUntil($destroy))
+      .subscribe(({ data }) => {
+        console.log('[Feature Convert] Lead data recived', data);
+        setLead(data ? data : null);
+      });
+  };
+
+  const convertLead = (id: string) => {
+    setScoreInProcess(true);
+    processLeadConvert(id)
+      .pipe(takeUntil($destroy))
       .subscribe((data) => {
-        console.log(data);
-        setLead(data);
+        console.log('[Feature Convert] process lead convert finished', data);
+        setScore(data);
+        setLead(
+          lead
+            ? {
+                ...lead,
+                hasScore: true,
+                score: data.score,
+              }
+            : null
+        );
+        setScoreInProcess(false);
       });
   };
 
   return (
-    <main className="container convert-container">
+    <main className="convert-container container">
       <h1 className="convert-container__title">
         {/* <button onClick={goToHome}>Go To Home</button> */}
         Start Process
@@ -53,19 +93,35 @@ export const SalesPipelineFeatureConvert: React.FC<
           type="text"
           className="search-lead__input"
           value={searchId}
+          placeholder="Lead ID"
           onChange={(e) => setSearchId(e.target.value)}
         />
         <button
           className="search-lead__button fill"
+          disabled={!searchId}
           onClick={() => getLeadById(searchId)}
         >
           Search
         </button>
       </section>
       <div className="lead-selected">
-        <Prospect lead={lead} />
+        <Prospect lead={lead} hasLeadSelected={hasLeadSelected()} />
       </div>
-      <button className="process-convert__button fill">Convert Lead</button>
+      <button
+        className="process-convert__button fill"
+        onClick={() => convertLead(searchId)}
+        disabled={!lead}
+      >
+        Process Score
+      </button>
+      <section className="convert-process-container">
+        <ConvertProcess
+          score={score}
+          scoreInProcess={scoreInProcess}
+          hasLeadSelected={hasLeadSelected()}
+          hasScore={hasScore()}
+        />
+      </section>
     </main>
   );
 };
