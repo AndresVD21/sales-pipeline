b049d3ebf10d6a28ce9ebf9ad1ec790babd4f234
Andres Valencia <andresvalencia.diaz@davita.com>
2022-09-24 23:26:37 -0500

    fix(shared): adding label prop to emoji component

--- libs/shared/src/lib/components/emoji/emoji.tsx
+++ libs/shared/src/lib/components/emoji/emoji.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable-next-line */
 export interface EmojiProps {
   emoji: string;
+  label: string;
 }
 
-export const Emoji: React.FC<EmojiProps> = ({ emoji }) => {
+export const Emoji: React.FC<EmojiProps> = ({ emoji, label }) => {
   return (
-    <span role="img" aria-label="hand pointing down">
+    <span role="img" aria-label={label}>
       {emoji}
     </span>
   );
