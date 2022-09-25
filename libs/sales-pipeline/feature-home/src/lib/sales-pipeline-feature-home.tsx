9aace97e3f1c58066c00e62869715e4ebad8eb44
Andres Valencia <andresvalencia.diaz@davita.com>
2022-09-24 23:29:11 -0500

    fix(lib-f-home): updating container style

--- libs/sales-pipeline/feature-home/src/lib/sales-pipeline-feature-home.tsx
+++ libs/sales-pipeline/feature-home/src/lib/sales-pipeline-feature-home.tsx
@@ -1,4 +1,4 @@
-import { Emoji } from '@sales-pipeline/shared';
+import { Emoji, Space } from '@sales-pipeline/shared';
 import { useNavigate } from 'react-router-dom';
 import './sales-pipeline-feature-home.scss';
 
@@ -15,11 +15,13 @@ export const SalesPipelineFeatureHome: React.FC<
   };
 
   return (
-    <main className="container">
+    <main className="container home-container">
       <h1 className="container__title">Welcome to Sales Pipeline!</h1>
       <section className="introduction">
         <p className="introduction__text">
-          Hi! Start the process to convert prospects below <Emoji emoji="👇🏾" />
+          Hi! Start the process to convert prospects below
+          <Space />
+          <Emoji emoji="👇🏾" label="hand pointing down" />
         </p>
         <button className="introduction__button" onClick={goToConvert}>
           Go To Convert
