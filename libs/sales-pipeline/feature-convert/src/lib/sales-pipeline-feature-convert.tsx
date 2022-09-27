14261ee0dccfc15755750e5ea79e053420793c5e
Andres Valencia <andresvalencia.diaz@davita.com>
2022-09-27 00:12:15 -0500

    update(libs): leaving nav link logic to shared component

--- libs/sales-pipeline/feature-convert/src/lib/sales-pipeline-feature-convert.tsx
+++ libs/sales-pipeline/feature-convert/src/lib/sales-pipeline-feature-convert.tsx
@@ -1,13 +1,11 @@
 import { CombinedScoreResponse, Lead } from '@sales-pipeline/data';
 import { getLead } from '@sales-pipeline/data-access';
 import { useEffect, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
 import styles from './sales-pipeline-feature-convert.module.scss';
 import { Subject, takeUntil } from 'rxjs';
 import Prospect from './components/prospects/prospects';
 import { processLeadConvert } from './services/convert.service';
 import ConvertProcess from './components/convert-process/convert-process';
-import { Nav } from '@sales-pipeline/shared';
 import Convert from './components/convert-process/convert/convert';
 
 const scoreInitialState = {
@@ -35,11 +33,6 @@ export const SalesPipelineFeatureConvert: React.FC<
   //     $destroy.unsubscribe();
   //   };
   // }, []);
-  const navigate = useNavigate();
-
-  const goToHome = () => {
-    navigate('/');
-  };
 
   const hasLeadSelected = () => {
     return !!lead;
@@ -87,75 +80,53 @@ export const SalesPipelineFeatureConvert: React.FC<
       });
   };
   return (
-    <>
-      <Nav>
-        <ul className={styles['nav__list']}>
-          <li className={styles['nav__list__item']}>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles['nav__list__item__link']} ${styles['link-active']}`
-                  : styles['nav__list__item__link']
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-        </ul>
-      </Nav>
-
-      <main className={styles['container']}>
-        <h1 className={styles['container__title']}>
-          {/* <button onClick={goToHome}>Go To Home</button> */}
-          Start Process
-        </h1>
-        <hr />
-        <p className={styles['container__info']}>
-          Please enter the lead ID you want to turn into prospects and press the
-          search button to retrieve the information.
-        </p>
-        <section className={styles['search-lead']}>
-          <input
-            type="text"
-            className={styles['search-lead__input']}
-            value={searchId}
-            placeholder="Lead ID"
-            onChange={(e) => setSearchId(e.target.value)}
-          />
-          <button
-            className={`${styles['button']} ${styles['search-lead__button']} ${styles['fill']}`}
-            disabled={!searchId}
-            onClick={() => getLeadById(searchId)}
-          >
-            Search
-          </button>
-        </section>
-        <section className={styles['lead-selected']}>
-          <Prospect
-            lead={lead}
-            hasLeadSelected={hasLeadSelected()}
-            leadNotFound={leadNotFound}
-          />
-        </section>
+    <main className={styles['container']}>
+      <h1 className={styles['container__title']}>Start Process</h1>
+      <hr />
+      <p className={styles['container__info']}>
+        Please enter the lead ID you want to turn into prospects and press the
+        search button to retrieve the information.
+      </p>
+      <section className={styles['search-lead']}>
+        <input
+          type="text"
+          className={styles['search-lead__input']}
+          value={searchId}
+          placeholder="Lead ID"
+          onChange={(e) => setSearchId(e.target.value)}
+        />
         <button
-          className={`${styles['button']} ${styles['process-score__button']} ${styles['fill']}`}
-          onClick={() => convertLead(searchId)}
-          disabled={!lead}
+          className={`${styles['button']} ${styles['search-lead__button']} ${styles['fill']}`}
+          disabled={!searchId}
+          onClick={() => getLeadById(searchId)}
         >
-          Process Score
+          Search
         </button>
-        <section className={styles['convert-process-container']}>
-          <ConvertProcess
-            score={score}
-            scoreInProcess={scoreInProcess}
-            hasLeadSelected={hasLeadSelected()}
-            hasScore={hasScore()}
-          />
-          {isAbleToConvert() && <Convert />}
-        </section>
-      </main>
-    </>
+      </section>
+      <section className={styles['lead-selected']}>
+        <Prospect
+          lead={lead}
+          hasLeadSelected={hasLeadSelected()}
+          leadNotFound={leadNotFound}
+        />
+      </section>
+      <button
+        className={`${styles['button']} ${styles['process-score__button']} ${styles['fill']}`}
+        onClick={() => convertLead(searchId)}
+        disabled={!lead}
+      >
+        Process Score
+      </button>
+      <section className={styles['convert-process-container']}>
+        <ConvertProcess
+          score={score}
+          scoreInProcess={scoreInProcess}
+          hasLeadSelected={hasLeadSelected()}
+          hasScore={hasScore()}
+        />
+        {isAbleToConvert() && <Convert />}
+      </section>
+    </main>
   );
 };
 
--- libs/sales-pipeline/feature-home/src/lib/sales-pipeline-feature-home.tsx
+++ libs/sales-pipeline/feature-home/src/lib/sales-pipeline-feature-home.tsx
@@ -1,4 +1,4 @@
-import { Emoji, Nav, Space } from '@sales-pipeline/shared';
+import { Emoji, Space } from '@sales-pipeline/shared';
 import { useNavigate } from 'react-router-dom';
 import styles from './sales-pipeline-feature-home.module.scss';
 
@@ -15,27 +15,22 @@ export const SalesPipelineFeatureHome: React.FC<
   };
 
   return (
-    <>
-      <Nav />
-      <main className={styles['container']}>
-        <h1 className={styles['container__title']}>
-          Welcome to Sales Pipeline!
-        </h1>
-        <section className={styles['introduction']}>
-          <p className={styles['introduction__text']}>
-            Hi! Start the process to convert prospects below
-            <Space />
-            <Emoji emoji="👇🏾" label="hand pointing down" />
-          </p>
-          <button
-            className={`${styles['button']} ${styles['introduction__button']} ${styles['fill']}`}
-            onClick={goToConvert}
-          >
-            Process Lead
-          </button>
-        </section>
-      </main>
-    </>
+    <main className={styles['container']}>
+      <h1 className={styles['container__title']}>Welcome to Sales Pipeline!</h1>
+      <section className={styles['introduction']}>
+        <p className={styles['introduction__text']}>
+          Hi! Start the process to convert prospects below
+          <Space />
+          <Emoji emoji="👇🏾" label="hand pointing down" />
+        </p>
+        <button
+          className={`${styles['button']} ${styles['introduction__button']} ${styles['fill']}`}
+          onClick={goToConvert}
+        >
+          Process Lead
+        </button>
+      </section>
+    </main>
   );
 };
 
